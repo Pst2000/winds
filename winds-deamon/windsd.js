10c3772769ba6cad16633815ad23418c179864ca
eachandall <eachandall@outlook.com>
2019-04-20 14:07:50 +0800

    deamon: parsing specific blocks

--- winds-deamon/windsd.js
+++ winds-deamon/windsd.js
@@ -1,25 +1,53 @@
-import GXClientFactory from "gxclient";
+'use strict';
+
+import GXClientFactory from 'gxclient';
 import { private_key, account_id, test_id } from './config';
 
 let client = GXClientFactory.instance({
     keyProvider: private_key,
     account: account_id,
-    network: "http://39.105.155.174:28090"
+    network: 'http://39.105.155.174:28090'
 });
 
-client.transfer(test_id, "", "0.5 GXC", true).then(trx => {
-    console.log("transfer success");
-}).catch(error => {
-    debugger;
-    if (error.code === 432) {
-        console.log("you don't authorize identity!");
-    }
-    console.error(error);
-});
+async function getBlockHeight() {
+    const prop = await client.getDynamicGlobalProperties();
+    return prop.head_block_number;
+}
+
+async function parseBlock(block) {
+    const result = [];
+    block.transactions.forEach(x => {
+        x.operations.forEach(op => {
+            const {account, method_name, data} = op[1];
+            if (method_name != 'push') return;
+            result.push({account, data: Buffer.from(data, 'hex').toString('utf8', 1)});
+        });
+    });
+    return result;
+}
+
+function generateQuery(blockID) {
+    return new Promise((resolve, reject) => {
+        client.getBlock(blockID).then(block => {
+            resolve(parseBlock(block));
+        });
+    });
+}
 
 async function main() {
-    const trx = await client.getContractTable("windstest1");
+    const pos = await getBlockHeight();
+    console.log(pos);
+
+    const input = [13045795, 13045212, 13045401, 13045302];
+    const obj = await Promise.all(input.map(generateQuery));
+    const list = obj.reduce((x, y) => x.concat(y));
+    list.forEach(x => {
+        console.log(x.account + ' ' + x.data);
+    });
+
+    return;
+    const trx = await client.getContractTable('windstest1');
     console.log(trx);
 }
 
-//main();
+main();
